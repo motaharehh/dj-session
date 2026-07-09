05b27b224dee010d316019426fc9f4d2a025849d
Motahareh <Motamiry777@gmail.com>
2026-07-09 12:55:01 +0330

    Add session model

--- session/models.py
+++ session/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Session(models.Model):
+    session_key = models.CharField(
+        max_length=64,
+        unique=True,
+    )
+
+    session_data = models.TextField(
+        default="",
+    )
+
+    def __str__(self):
+        return self.session_key
